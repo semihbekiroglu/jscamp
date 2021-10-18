d0c7c70061eff0293e9722c3e752a9282497b992
semihbekiroglu <76533016+semihbekiroglu@users.noreply.github.com>
2021-10-19 00:14:30 +0300

    18ekimderssonu

---
+++ src/app.js
@@ -0,0 +1,10 @@
+let konutKredileri=["Konut Kredisi", "emlak konut kredisi","kamu konut kredisi"]
+
+console.log("<ul>")
+for (let i=0;i<konutKredileri.length;i++) {
+
+console.log("<li>"+konutKredileri[i]+"</li>")
+
+
+}
+console.log("</ul>")
